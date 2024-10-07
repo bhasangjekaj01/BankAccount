fb3173be8a4900c3bdd7465bdbc386a06b1f233a
bhasangjekaj01 <bhasangjekaj01@bvsd.org>
2024-10-07 11:17:36 -0600

    in progress

--- BankAccount.java
+++ BankAccount.java
@@ -17,7 +17,7 @@ public class BankAccount {
     this.loggedIn = true;
   }
 
-
+//add bank information
   public boolean logIn(String name, String password)
   {
     if(name.equals(this.name) && password.equals(this.password))
@@ -45,14 +45,20 @@ public class BankAccount {
 
   }
 
-  //public double withdraw(double amount)
-  //{
-  //}
+  public static double Withdraw(double balance, double withdrawnAmount)
+  {
+    System.out.println("Withdrawing amount: " + withdrawnAmount);
+    if (balance >= withdrawnAmount) {
+      balance = balance - withdrawnAmount;
+      System.out.println("Withdrawal complete");
+      System.out.println("Current balance: " + balance);
+    }
+    else{
+      System.out.println();
 
-  //public double deposit(double amount)
-  //{
+    }
 
-  //}
+  }
 
   public String toString()
   {
